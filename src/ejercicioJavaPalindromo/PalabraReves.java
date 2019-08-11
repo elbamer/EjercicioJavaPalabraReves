7816ed5cfd7cb0b8a1b9c1a91c25fd34065ad7af
elbamer <elbamer33@gmail.com>
2019-08-11 12:21:45 +0200

    Original

---
+++ src/ejercicioJavaPalindromo/PalabraReves.java
@@ -0,0 +1,26 @@
+package ejercicioJavaPalindromo;
+
+import java.util.Arrays;
+/**
+ *
+ * @Elba
+ */
+
+public class PalabraReves {
+	public static char[] letraReves(String palab1){
+		int letras = palab1.length();
+		char [] let1 = new char [letras];   
+		for (int i=letras-1; i>-1; i--){
+			let1[letras-1-i]=palab1.charAt(i);
+
+		}
+		return let1;
+	}	    
+
+	public static void main(String[] args) {        
+		//Escribe la palabra al reves.
+		String palabra1="Elba";
+		char [] reves=letraReves(palabra1);
+		System.out.println("La palabra al reves es: "+Arrays.toString(reves).toUpperCase());	        
+	}
+}
